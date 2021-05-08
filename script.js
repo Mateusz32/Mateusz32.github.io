3819051bbcbf1ebea8d55da3c97329b73dae0587
Mateusz32 <73787261+Mateusz32@users.noreply.github.com>
2021-05-08 20:17:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
    const apiRoot = 'https://blooming-scrubland-00919.herokuapp.com/v1/task/';
-   const trelloApiRoot = 'https://api.trello.com/1/members/mateusz_19@o2.pl/boards?key=a52b0319c84a238fe4d8bb3def9324d7&token=e6fca857d53a11b2268bfcd47dc864b000d2d4d7ed58d61fa1b7d5ca96f376ca';
+   const trelloApiRoot = 'https://blooming-scrubland-00919.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
